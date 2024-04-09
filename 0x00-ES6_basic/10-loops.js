43c4b1655003c49b35816c918ef261b2fe077bd9
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-09 02:33:19 +0200

    update

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,17 +1,8 @@
-// Original function
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+  const array = [];
+  for (const value of array) {
+    array.push(appendString + value);
   }
 
   return array;
 }
-
-// Input array
-const inputArray = ['appended', 'fixed', 'displayed'];
-
-// Call the function and store the result
-const result = appendToEachArrayValue(inputArray, 'correctly-');
-
-// Log the result to verify
-console.log(result);
