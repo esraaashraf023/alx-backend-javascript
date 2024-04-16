a58bd6e11ed5c0dfb2a9cda5bcd115d2289933a9
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-16 20:56:23 +0200

    يارب يطلع صح

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,14 @@
-function updateStudentGradeByCity(studentList, city, newGrades) {
-  return studentList
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      const grade = gradeObj ? gradeObj.grade : 'N/A';
-      return { ...student, grade };
-    });
-}
+const updateStudentGradeByCity = (studentList, city, newGrades) => {
+  const idGradMap = new Map();
+  newGrades.forEach((student) => {
+    idGradMap.set(student.studentId, student.grade);
+  });
+
+  const studentsByCity = studentList.filter((student) => (student.location === city));
+  return studentsByCity.map((student) => ({
+    ...student,
+    grade: idGradMap.get(student.id) || 'N/A',
+  }));
+};
 
 export default updateStudentGradeByCity;
