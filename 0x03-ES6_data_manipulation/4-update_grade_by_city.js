a87feeba49a220ab64d38d312504a4a1538f8b51
esraaashraf023 <esraaashraf780567@gmail.com>
2024-04-16 20:49:11 +0200

    add file 3

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
+
+export default updateStudentGradeByCity;
